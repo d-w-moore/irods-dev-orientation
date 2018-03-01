7eb424e4e8d3ed1bc7647253a91e9d2e988350f8
d-w-moore <35236229+d-w-moore@users.noreply.github.com>
2018-03-01 06:20:45 -0500

    Update syncbuildabear.sh

--- syncbuildabear.sh
+++ syncbuildabear.sh
@@ -5,14 +5,15 @@
 # Save as ~/bin/syncbuildabear
 # Replicate the lines below to sync more repo's
 # The SHA for the curent repo will be printed by each "git rev-parse"
+# use 'R_USERNAME=<remote_loginname> ~/bin/syncbuildabear' if remote != local 
 ########################
 
 BUILDHOST=build-ub12.irods.renci.org
 SOURCEPATH=/home/${USERNAME:=`id -un`}/Tmp/irods
-TARGETPATH=/projects/irods/personal-testing-repos/dmoore/irods
+TARGETPATH=/projects/irods/personal-testing-repos/${R_USERNAME:=$USERNAME}/irods
 
 echo "Syncing irods to ${BUILDHOST}..."
-rsync -rlt --delete ${SOURCEPATH}/.git/ ${USERNAME:-`id -un`}@${BUILDHOST}:${TARGETPATH}/.git/
+rsync -rlt --delete ${SOURCEPATH}/.git/ ${R_USERNAME}@${BUILDHOST}:${TARGETPATH}/.git/
 echo "------ Build-a-Bear Parameters ------"
 echo ${TARGETPATH}
 cd ${SOURCEPATH}
