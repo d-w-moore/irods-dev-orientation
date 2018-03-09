9fe1802a658b5050fad80c02794dc0dc4f2a70e8
d-w-moore <35236229+d-w-moore@users.noreply.github.com>
2018-03-09 17:48:32 -0500

    Update syncbuildabear.sh

--- syncbuildabear.sh
+++ syncbuildabear.sh
@@ -3,37 +3,33 @@
 ########################
 # iRODS Build-a-Bear v4
 # Save as ~/bin/syncbuildabear
-# Replicate the lines below to sync more repo's
-# The SHA for the curent repo will be printed by each "git rev-parse"
-# use 'R_USERNAME=<remote_loginname> syncbuildabear' if remote != local 
+# run as "R_USER=remoteLoginName syncbuildabear" if remoteLoginName NE `id -un`
 ########################
 
-PATH_TO_REPOS=$HOME/github #__ change to reflect your own choice of local path
+RELPATH_TO_REPOS=./github  #__ directory / path undef $HOME containing your repo's
 
 BUILDHOST=build-ub12.irods.renci.org
-SOURCEPATH=/home/${USERNAME:=`id -un`}/${PATH_TO_REPOS}/irods  #__ server code
-TARGETPATH=/projects/irods/personal-testing-repos/${R_USERNAME:=$USERNAME}/irods
+SOURCEPATH=/home/${USERNAME:=`id -un`}/${RELPATH_TO_REPOS}/irods
+TARGETPATH=/projects/irods/personal-testing-repos/${R_USER:=$USERNAME}/irods
 
 echo "Syncing irods to ${BUILDHOST}..."
-rsync -rlt --delete ${SOURCEPATH}/.git/ ${R_USERNAME}@${BUILDHOST}:${TARGETPATH}/.git/
+rsync -rlt --delete ${SOURCEPATH}/.git/ ${R_USER}@${BUILDHOST}:${TARGETPATH}/.git/
 echo "------ Build-a-Bear Parameters ------"
 echo ${TARGETPATH}
 cd ${SOURCEPATH}
 git rev-parse HEAD
 
-#==  Optionally, copy the above section as below to build/test a custom 
-#    irods_client_icommands repo. (Do so by removing or commenting out the `cat` line which follows)
-cat <<':'>/dev/null
+#== replicate for other repository as necessary
 
 BUILDHOST=build-ub12.irods.renci.org
-SOURCEPATH=/home/${USERNAME:=`id -un`}/${PATH_TO_REPOS}/irods  #__ server code
-TARGETPATH=/projects/irods/personal-testing-repos/${R_USERNAME:=$USERNAME}/irods_client_icommands
+SOURCEPATH=/home/${USERNAME:=`id -un`}/${RELPATH_TO_REPOS}/irods_client_icommands
+TARGETPATH=/projects/irods/personal-testing-repos/${R_USER:=$USERNAME}/irods_client_icommands
 
-echo "Syncing irods to ${BUILDHOST}..."
-rsync -rlt --delete ${SOURCEPATH}/.git/ ${R_USERNAME}@${BUILDHOST}:${TARGETPATH}/.git/
-echo "------ Build-a-Bear Parameters ------"
-echo ${TARGETPATH}
-cd ${SOURCEPATH}
-git rev-parse HEAD
-
-:
+if [ -d "${SOURCEPATH}" ]; then
+  echo "Syncing irods to ${BUILDHOST}..."
+  rsync -rlt --delete ${SOURCEPATH}/.git/ ${R_USER}@${BUILDHOST}:${TARGETPATH}/.git/
+  echo "------ Build-a-Bear Parameters ------"
+  echo ${TARGETPATH}
+  cd ${SOURCEPATH}
+  git rev-parse HEAD
+fi
